88592f383f9e39ab14e9e64da28173e13066ab99
Ouriel91 <orieloh91@gmail.com>
2020-03-29 22:53:27 +0300

    Added toolbar , logo and navigation items

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Aux from '../../hoc/Auxilliary'
 import classes from './Layout.module.css'
+import Toolbar from '../Navigation/Toolbar/Toolbar'
 
 //The content inside the <main> element should be unique to the document. 
 //It should not contain any content that is repeated across documents such as 
 //sidebars, navigation links, copyright information, site logos, and search forms.
 const layout = (props) => (
    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
