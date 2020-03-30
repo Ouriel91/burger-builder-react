7cc63de48895f31a36bee7f4c88d48dac5a65c54
Ouriel91 <orieloh91@gmail.com>
2020-03-30 22:54:47 +0300

    Added toggle button to close and open sideDrawer

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Aux from '../../hoc/Auxilliary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
@@ -7,15 +7,36 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 //The content inside the <main> element should be unique to the document. 
 //It should not contain any content that is repeated across documents such as 
 //sidebars, navigation links, copyright information, site logos, and search forms.
-const layout = (props) => (
-   <Aux>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-   </Aux>
-)
+
+//Turn layout to class based component to trigger backdrop
+class Layout extends Component {
+
+    state ={
+        showSideDrawer: false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    sideDrawerToggleHandler= () => {
+        this.setState( (prevState) => { //prevent a problems with async code (this state depends in the old state)
+            return {showSideDrawer: !prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
+}
 
 
-export default layout
+export default Layout
